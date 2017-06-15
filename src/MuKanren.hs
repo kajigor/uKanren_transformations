f3f3bdb835428c6457487a96c8977c91f5454faa
Dmitry Boulytchev <dboulytchev@gmail.com>
2017-06-16 01:38:26 +0300

    Sample interpreter

--- src/MuKanren.hs
+++ src/MuKanren.hs
@@ -1,23 +1,89 @@
-{-
-   Implementation of MicroKanren as in the paper
-   "µKanren: A Minimal Functional Core for Relational Programming"
-   by Jason Hemann and Daniel P. Friedman
--}
-
-module MuKanren where
 import Debug.Trace
 import Data.List
+import Control.Monad
+import Control.Applicative
+import Data.Maybe 
 
-type Var = Int
--- for now, only integer atoms are allowed
-data Term = Var Var | Pair Term Term | Atom Int | Nil | R Var deriving Eq
+-- Abstraction synonyms
+type Name = String
+type Var  = Int
 
-instance Show Term where
-  show (Var  v)   = "x." ++ show v
-  show (Pair v u) = "(" ++ show v ++ ", " ++ show u ++ ")"
-  show (Atom i)   = show i
-  show Nil        = "[]"
-  show (R v)      = "_." ++ show v
+-- Terms
+data Term = C Name [Term] | L Var | V Name deriving (Eq, Show)
+
+-- Goals
+data G = 
+    Term :=: Term
+  | G :&: G
+  | G :!: G
+  | Fresh  Name G 
+  | Invoke Name [Term] deriving Show
+
+infixr 7 &&&
+infixr 6 |||
+infix  8 ===
+
+(===) = (:=:)
+(|||) = (:!:)
+(&&&) = (:&:)
+
+fresh xs g = foldr Fresh g xs
+call       = Invoke
+
+-- Definitions
+type D = (Name, [Name], G)
+
+def = (,,)
+
+-- Specification
+type S = ([D], G)
+
+spec = (,)
+
+-- Interpreter
+type State = (String -> Term, [(Var, Term)], Int)
+
+update bnds x v y = if y == x then v else bnds y
+
+subst  st         (C n ts) = C n $ map (subst st) ts
+subst  (st, _, _) (V n)    = st n
+subst  _          t        = t
+
+substG st                   (t1 :=: t2)   = (subst  st t1) :=: (subst  st t2)
+substG st                   (g1 :!: g2)   = (substG st g1) :!: (substG st g2)
+substG st                   (g1 :&: g2)   = (substG st g1) :&: (substG st g2)
+substG (bnds, subst, index) (Fresh x g)   = Fresh x (substG (update bnds x (V x), subst, index) g)
+substG st                   (Invoke f as) = Invoke f (map (subst st) as)
+
+eval env state (t1 :=: t2)   = maybeToStream $ unify (Just state) (subst state t1) (subst state t2)
+eval env state (g1 :!: g2)   = eval env state g1 `mplus` eval env state g2
+eval env state (g1 :&: g2)   = eval env state g1 >>= (\ st -> eval env st (substG state g2))
+eval env state (Fresh x g)   = eval env (freshVar state x) g
+eval env state (Invoke f as) = 
+  let (_, fs, g) = env f in
+  let state'     = foldl bindVar state $ zip fs as in
+  eval env state' g
+
+bindVar  st@(bnds, sub, index) (fa, aa) = (update bnds fa (subst st aa), sub, index  )
+freshVar st@(bnds, sub, index) x        = (update bnds x  (L index)    , sub, index+1)
+
+unify Nothing _ _ = Nothing
+unify st@(Just (s@(bnds, subst, index))) u v = 
+  unify' (walk u subst) (walk v subst)  where
+    unify' (L u) (L v) | u == v = Just s
+    unify' (L u) _              = Just (bnds, extS u v subst, index)
+    unify' _ (L v)              = Just (bnds, extS v u subst, index)
+    unify' (C a as) (C b bs) | a == b && length as == length bs = foldl (\ st (u, v) -> unify st u v) st $ zip as bs
+    unify' (V _) _ = error "syntactic variable in unification"
+    unify' _ (V _) = error "syntactic variable in unification"
+    unify' _ _ = Nothing
+    walk x@(L v) s =
+      case lookup v s of
+        Nothing -> x
+        Just t  -> walk t s
+    walk (V _) _ = error "syntactic variable in walk"
+    walk u     _ = u
+    extS u v s = (u, v) : s
 
 data Stream a = Empty
               | Mature a (Stream a)
@@ -25,143 +91,104 @@ data Stream a = Empty
               | Immature (Stream a)
               deriving Show
 
-type ESubst = [(Var, Either AST Term)]
+takeS 0 _            = []
+takeS n Empty        = []
+takeS n (Mature a s) = a : takeS (n-1) s
+takeS n (Immature s) = takeS n s
 
-type Subst = ([(Var, Term)], Int)
+maybeToStream Nothing  = Empty
+maybeToStream (Just a) = return a
 
-type Name = String
-data AST = Uni Term Term
-         | Conj AST AST
-         | Disj AST AST
-         | Fresh (Term -> AST)
-         | Fun Name AST
-         | Call AST [Term]
-         | Zzz AST
-         | GV Var ESubst Int
+instance Functor Stream where
+  fmap _ Empty        = Empty
+  fmap f (Mature a s) = Mature (f a) (fmap f s)
+  fmap f (Immature s) = Immature (fmap f s)
 
-instance Show AST where
-  show (Uni  x y) = "(" ++ show x ++ " === " ++ show y ++ ")"
-  show (Conj x y) = "(" ++ show x ++ " &&& " ++ show y ++ ")"
-  show (Disj x y) = "(" ++ show x ++ " ||| " ++ show y ++ ")"
-  show (Fun  n b) = "(" ++ n ++ "(" ++ show b ++ ")" ++ ")"
-  show (Fresh  f) = "(" ++ "fresh" ++ ")"
-  show (Call (Fun n _) arg) = "(" ++ "call " ++ n ++ " with [" ++ concatMap (\x -> show x ++ "; ") arg ++ "]" ++ ")"
-  show (Zzz a) = "zzz " ++ show a ++ ")"
-  show (GV v _ _) = "v." ++ show v
+instance Applicative Stream where
+  pure a = Mature a Empty
+  Empty        <*> _            = Empty
+  (Mature f s) <*> Empty        = Empty
+  (Immature s) <*> x            = s <*> x
+  (Mature f s) <*> (Mature x t) = Mature (f x) (s <*> t)
+  s            <*> (Immature t) = s <*> t
 
--- Combinators to write programs with
-var = Var
-at = Atom
+instance Alternative Stream where
+  empty = Empty
+  Empty <|> s = s
+  s <|> Empty = s
+  (Immature s) <|> t = s <|> t
+  s <|> (Immature t) = s <|> t
+  (Mature a s) <|> t = Mature a (s <|> t)
 
-(===) = Uni
-(|||) = Disj
-(&&&) = Conj
-callFresh = Fresh
-fun = Fun
-zzz = Zzz
-nil = Nil
-pair = Pair
-call = Call
-list xs = foldr pair nil xs
+instance Monad Stream where
+  Empty >>= _ = mzero
+  (Mature x xs) >>= g = g x `mplus` (xs >>= g)
+  (Immature _1) >>= _2 = Immature (_1 >>= _2)
 
-seq2 f [x]    = x
-seq2 f (x:xs) = x `f` seq2 f xs
+instance MonadPlus Stream where
+  mzero = Empty
+  mplus (Mature h tl) _2 = Mature h (_2 `mplus` tl)
+  mplus (Immature _1) _2 = Immature (_2 `mplus` _1)
+  mplus Empty         _2 = _2
 
-conj = seq2 (&&&)
-disj = seq2 (|||)
+state0 = ((\ _ -> undefined), [], 0)
 
-conde ds = disj (map conj ds)
+run :: S -> Stream State
+run (defs, goal) =
+  let defs' = map (\ d@(n, _, _) -> (n, d)) defs in
+  eval (\ n -> fromJust $ lookup n defs') state0 goal
 
-emptySubst = []
-emptyState= (emptySubst, 0 :: Int)
+-- Tests
+nil      = C "Nil"  []     
+cons x y = C "Cons" [x, y] 
+i    x   = C x      []
 
-mzero = Empty
+appendo =
+  let x  = V "x"  in
+  let y  = V "y"  in
+  let xy = V "xy" in
+  let h  = V "h"  in
+  let t  = V "t"  in
+  let ty = V "ty" in
+  def "appendo" ["x", "y", "xy"] 
+         ((x === nil &&& xy === y) ||| 
+          (fresh ["h", "t", "ty"] 
+             (x  === h `cons` t  &&&
+              xy === h `cons` ty &&&
+              call "appendo" [t, y, ty]
+             )
+          )
+         )
 
--- used for disjunctions, interleaves streams
-mplus Empty _2 = _2
-mplus (Mature h tl) _2 = Mature h (_2 `mplus` tl)
-mplus (Immature _1) _2 = Immature (_2 `mplus` _1)
+reverso =
+  let x  = V "x"  in
+  let y  = V "y"  in
+  let h  = V "h"  in
+  let t  = V "t"  in
+  let rt = V "rt" in
+  def "reverso" ["x", "y"]
+         ((x === nil &&& y === nil) |||
+          (fresh ["h", "t", "rt"]
+             (x === h `cons` t &&&
+              call "appendo" [rt, h `cons` nil, y] &&&
+              call "reverso" [t, rt] 
+             )
+          )
+         )
 
--- used for conjuctions
-bind Empty g = mzero
-bind (Mature x xs) g = g x `mplus` bind xs g
-bind (Immature _1) _2 = Immature (bind _1 _2)
+toplevel n spec = takeS n $ fmap (\ (_, s, _) -> s) (run spec)
 
-walk (Var v) s =
-  case lookup v s of
-    Nothing -> Var v
-    Just t  -> walk t s
-walk u _ = u
+main = 
+  do
+    putStrLn $ show (toplevel 1 ([appendo], fresh ["q"] (call "appendo" [nil, nil, V "q"])))
+    putStrLn $ show (toplevel 1 ([appendo], fresh ["q"] (call "appendo" [i "A" `cons` nil, nil, V "q"])))
+    putStrLn $ show (toplevel 1 ([appendo], fresh ["q"] (call "appendo" [i "A" `cons` nil, i "B" `cons` nil, V "q"])))
+    putStrLn $ show (toplevel 1 ([appendo], fresh ["q"] (call "appendo" [ V "q", i "B" `cons` nil, i "A" `cons` (i "B" `cons` nil)])))
+    putStrLn $ show (toplevel 1 ([appendo, reverso], fresh ["q"] (call "reverso" [nil, V "q"])))
+    putStrLn $ show (toplevel 1 ([appendo, reverso], fresh ["q"] (call "reverso" [i "A" `cons` nil, V "q"])))
+    putStrLn $ show (toplevel 1 ([appendo, reverso], fresh ["q"] (call "reverso" [i "A" `cons` (i "B" `cons` nil), V "q"])))
+    putStrLn $ show (toplevel 1 ([appendo, reverso], fresh ["q"] (call "reverso" [V "q", i "A" `cons` (i "B" `cons` nil)])))
+    
 
-extS u v s =
-  (u, v) : s
-  {- let v' = walk v s
-      occurs_check (Var x) (Var v) = x == v
-      occurs_check x (Pair v u) = occurs_check x v s || occurs_check x u s
-      occurs_check _ _ = False
-  in
-  case occurs_check u v' s of
-    False -> Just ((u, v) : s)
-    True  -> Nothing -}
-
-unify u v s =
-  -- do we need to add occurs check?
-  unify' (walk u s) (walk v s)
-  where
-    unify' (Var u) (Var v) | u == v = Just s
-    unify' (Var u) _ = Just (extS u v s)
-    unify' _ (Var v) = Just (extS v u s)
-    unify' (Pair u u') (Pair v v') =
-      case unify u v s of
-        Nothing -> Nothing
-        Just s' -> unify u' v' s'
-    unify' (Atom u) (Atom v) | u == v = Just s
-    unify' Nil Nil = Just s
-    unify' _ _ = Nothing
-
-showSt (s,c) =
-  let reified = sortBy (\(x,_) (y,_) -> if x < y then LT else if x == y then EQ else GT) $ map (\(x,v) -> (x, walk' v s)) s
-  in show (reified, c)
-
--- program evaluates to a stream of states which are pairs of substitution and
--- auxilary variable counter.
-eval x st@(s,c) =
-  case x of
-    (Uni t t') ->
-      let s' = unify t t' s
-          unit (s,c) = Mature (s,c) Empty
-      in
-      case s' of
-        Nothing -> mzero
-        Just s' -> unit (s',c)
-    (Disj g g') -> eval g st `mplus` eval g' st
-    (Conj g g') -> eval g st `bind` \st -> eval g' st
-    (Fresh f) -> eval (f (var c)) (s, c + 1)
-    (Fun _ a) -> eval a st
-    (Call f _) -> eval f st
-    (Zzz a) -> Immature (eval a st)
-
-reify' v stream =
-  let map' f Empty = []
-      map' f (Mature x xs) = f x : map' f xs
-  in map' (\(s,c) -> reify v s) stream
-
-walk' v s =
-  case walk v s of
-    Var u -> Var u
-    Pair v u -> Pair (walk' v s) (walk' u s)
-    u -> u
-
-reify v s =
-  let
-      u = walk' v s
-
-      reify_s v s =
-        case walk v s of
-          Var v -> extS v (reify_name $ length s) s
-          Pair v u -> reify_s u (reify_s v s)
-          _ -> s
-
-      reify_name = R -- temporary solution. need to get rid of it
-        --"_." ++ (show n)
-  in walk' u (reify_s u emptySubst)
+    
+  
