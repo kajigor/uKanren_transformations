26cc13c3bd73df45934acbf47af65c0740e35c28
Ekaterina Verbitskaia <kajigor@gmail.com>
2020-06-09 17:43:31 +0300

    Tests reorganization

--- test/auto/Test/Helper.hs
+++ test/auto/Test/Helper.hs
@@ -7,9 +7,13 @@ module Test.Helper
   , test4
   , test5
   , manyAssert
+  , assertCustom
+  , manyAssertCustom
   ) where
 
-import           Test.Tasty.HUnit (Assertion, (@?=))
+import           Data.List        (find)
+import           Data.Maybe       (isJust)
+import           Test.Tasty.HUnit (Assertion, assertBool, (@?=))
 
 test :: (Eq b, Show b) => (a -> b) -> a -> b -> Assertion
 test f input output =
@@ -34,3 +38,11 @@ test5 f input =
 manyAssert :: (Eq a, Show a) => a -> (b -> с -> a) -> [(b, с)] -> Assertion
 manyAssert expected f =
   mapM_ (\(x, y) -> test2 f x y expected)
+
+assertCustom :: (Eq a, Show a) => String -> (a -> b -> Bool) -> a -> b -> Assertion
+assertCustom str fun x y =
+  assertBool str (fun x y)
+
+manyAssertCustom :: (Eq a, Show a) => String -> (a -> a -> Bool) -> [a] -> [a] -> Assertion
+manyAssertCustom str check expected actual =
+    assertBool str (all (\a -> isJust $ find (check a) expected) actual)
