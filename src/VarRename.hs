be7384fb8687586f525974f59293356188403722
Ekaterina Verbitskaia <kajigor@gmail.com>
2023-01-31 22:14:57 +0100

    Clashing of variables when flattening the goals fixed

--- src/VarRename.hs
+++ src/VarRename.hs
@@ -1,4 +1,4 @@
-module VarRename (uniquelyRenameVars) where
+module VarRename where
 
 import           Control.Monad.State
 import qualified Data.Map            as M
@@ -8,27 +8,28 @@ import           Program
 import           Syntax
 
 data RenameState a = RenameState
-  { getVarMap     :: M.Map X a
-  , getNameSource :: PolyFreshNames a
+  { getVarMap       :: M.Map X a
+  , getNameSource   :: PolyFreshNames a
+  , getFirstNotUsed :: PolyFreshNames a
   }
 
 emptyState :: FreshName a => RenameState a
-emptyState = RenameState { getVarMap = M.empty, getNameSource = defaultNames }
+emptyState = RenameState { getVarMap = M.empty, getNameSource = defaultNames, getFirstNotUsed = defaultNames }
 
 uniquelyRenameVars :: Program G X -> Maybe (Program G S, S)
 uniquelyRenameVars (Program defs goal) = do
   (goal, gState) <- runStateT (enumerate goal) emptyState
   defs <- mapM (\d -> runStateT (enumerateDef d) emptyState) defs
-  let highestNextVar = maximum (getNameSource gState : map (getNameSource . snd) defs)
-  return $ (Program (map fst defs) goal, fst $ getFreshName highestNextVar)
+  let highestNextVar = maximum (getFirstNotUsed gState : map (getFirstNotUsed . snd) defs)
+  return (Program (map fst defs) goal, fst $ getFreshName highestNextVar)
 
-enumerateDef ::  (Show a, FreshName a) => Def G X -> StateT (RenameState a) Maybe (Def G a)
+enumerateDef ::  (Ord a, Show a, FreshName a) => Def G X -> StateT (RenameState a) Maybe (Def G a)
 enumerateDef (Def name args body) = do
   args <- mapM newVar args
   body <- enumerate body
   return $ Def name args body
 
-enumerate :: (Show a, FreshName a) => G X -> StateT (RenameState a) Maybe (G a)
+enumerate :: (Ord a, Show a, FreshName a) => G X -> StateT (RenameState a) Maybe (G a)
 enumerate (x :=: y) = do
   x <- enumerateTerm x
   y <- enumerateTerm y
@@ -46,25 +47,25 @@ enumerate (Fresh x g) = do
   (g, v') <- withNewName x (enumerate g)
   return (Fresh v' g)
 
-withNewName :: FreshName a =>
+withNewName :: (Ord a, FreshName a) =>
                X ->
                StateT (RenameState a) Maybe b ->
                StateT (RenameState a) Maybe (b, a)
 withNewName x f = do
-  oldState@(RenameState varMap nameSource) <- get
+  oldState@(RenameState varMap nameSource firstNotUsed) <- get
   let (newName, newSource) = getFreshName nameSource
   let newVarMap = M.insert x newName varMap
-  put (RenameState newVarMap newSource)
+  put (RenameState newVarMap newSource (max newSource firstNotUsed))
   r <- f
-  put oldState
+  put $ oldState { getFirstNotUsed = max newSource firstNotUsed}
   return (r, newName)
 
-newVar :: FreshName a => X -> StateT (RenameState a) Maybe a
+newVar :: (Ord a, FreshName a) => X -> StateT (RenameState a) Maybe a
 newVar x = do
-  oldState@(RenameState varMap nameSource) <- get
+  oldState@(RenameState varMap nameSource firstNotUsed) <- get
   let (newName, newSource) = getFreshName nameSource
   let newVarMap = M.insert x newName varMap
-  put (RenameState newVarMap newSource)
+  put (RenameState newVarMap newSource (max newSource firstNotUsed))
   return newName
 
 enumerateVar :: (Show a, FreshName a) => X -> StateT (RenameState a) Maybe a
