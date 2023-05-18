86e39fc256a4727fcfc06857135e9b42e6b19aed
gt22 <igorengel@mail.ru>
2023-05-18 14:06:19 +0200

    cleanup

--- src/FunConversion/Syntax.hs
+++ src/FunConversion/Syntax.hs
@@ -1,15 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE FunctionalDependencies #-}
 
 module FunConversion.Syntax where
 import           Util.String
 import qualified Language.Haskell.TH as TH
-import Debug.Trace
 
 type Var = String
 type Name = String
-type Op = String
 type Generator = String
 
 data Term = Var Var
@@ -19,20 +15,20 @@ data Delayed = Delayed
              | NotDelayed
              deriving (Show, Eq)
 
-data Lang = Call Delayed Name [Var] [Generator]
+data Lang = Empty
+          | Call Delayed Name [Var] [Generator]
           | Return [Term]
           | Sum [Lang]
           | Match Var [(Term, Lang)]
           | Bind [([Var], Lang)]
-          | Guard Var Var 
+          | Guard Var Term
           | Gen Generator deriving (Show, Eq)
 
-data Def = Def Name ([Term], [Generator], Lang) deriving (Show, Eq)
-
+data Def = Def { name :: Name, args :: [Var], generators :: [Generator], body :: Lang } deriving (Show, Eq)
 
 newtype TypeData = TypeData [(String, Int)] deriving (Show, Eq)
 
-data Program = Program { types :: TypeData, defs :: [Def], body :: Maybe Lang } deriving (Show, Eq)
+data Program = Program { types :: TypeData, defs :: [Def], goal :: Maybe Lang } deriving (Show, Eq)
 
 data ProgramDec = ProgramDec { decs :: [TH.Dec], callGoal :: Maybe TH.Exp }
 
@@ -42,40 +38,48 @@ class Quotable a q | a -> q where
   toQuote :: a -> Either Error q
 
 -- TODO: Separate type from function
-embedProg :: String -> Either String Program -> [TH.Dec]
-embedProg n (Right p@(Program _ _ call)) = case toQuote p of
+embedProg :: String -> Either Error Program -> [TH.Dec]
+embedProg n p = case p >>= embedProgSafe n of
   Left e -> error e
-  Right (ProgramDec decs body) -> callToDec n call body ++ decs
-embedProg n e = error $ "Invalid prog for embed: " ++ show e
+  Right p -> p
 
-embedProgSafe :: String -> Program -> Either String [TH.Dec]
-embedProgSafe n p@(Program _ _ call) = case toQuote p of
-  Left e -> Left e
-  Right (ProgramDec decs body) -> Right $ decs ++ callToDec n call body
+embedProgSafe :: String -> Program -> Either Error [TH.Dec]
+embedProgSafe n p@(Program _ _ call) = do
+  (ProgramDec decs body) <- toQuote p
+  call <- callToDec n call body
+  return $ decs ++ call
 
-callToDec :: String -> Maybe Lang -> Maybe TH.Exp -> [TH.Dec]
-callToDec n (Just (Call _ _ args gens)) (Just body) = [TH.FunD (TH.mkName n) [TH.Clause (map pvar args ++ map pgen gens) (TH.NormalB body) []]]
-callToDec _ _ _ = []
+callToDec :: String -> Maybe Lang -> Maybe TH.Exp -> Either Error [TH.Dec]
+callToDec n (Just (Call _ _ args gens)) (Just body) = do
+  args <- mapM pvar args
+  gens <- mapM pgen gens
+  return [TH.FunD (TH.mkName n) [TH.Clause (args ++ gens) (TH.NormalB body) []]]
+callToDec _ _ _ = return []
 
 qvar :: Var -> Either Error TH.Exp
 qvar v
     | null v = Left "Var name cannot be empty"
-    | otherwise = Right $ TH.VarE $ TH.mkName $ toLower v
+    | otherwise = Right $ TH.VarE $ TH.mkName v
+
+pvar :: Var -> Either Error TH.Pat
+pvar v 
+    | null v = Left "Var name cannot be empty"
+    | otherwise = Right $ TH.VarP $ TH.mkName v
 
 qgen :: Generator -> Either Error TH.Exp
-qgen v
-    | null v = Left "Gen name cannot be empty"
-    | otherwise = Right $ TH.VarE $ TH.mkName $ toLower v
+qgen = qvar
 
-pgen :: Generator -> TH.Pat 
-pgen v = TH.VarP $ TH.mkName $ toLower v
+pgen :: Generator -> Either Error TH.Pat
+pgen = pvar
 
-pvar :: Var -> TH.Pat
-pvar v = TH.VarP $ TH.mkName $ toLower v
+qname :: Name -> Either Error TH.Exp
+qname = qvar
 
-pterm :: Term -> TH.Pat
+pterm :: Term -> Either Error TH.Pat
 pterm (Var v) = pvar v
-pterm (Con n args) = TH.ConP (TH.mkName n) [] (map pvar args)
+pterm (Con n args) = do
+  args <- mapM pvar args
+  return $ TH.ConP (TH.mkName n) [] args
 
 instance Quotable Term TH.Exp where
   toQuote (Var v) = qvar v
@@ -87,53 +91,58 @@ instance Quotable Term TH.Exp where
       return $ TH.ConE (TH.mkName con) $: ts
 
 instance Quotable Def TH.Dec where
-  toQuote (Def name def) = do
-    ds <- go def
-    return $ TH.FunD (TH.mkName name) [ds]
+  toQuote d = do
+    ds <- go d
+    return $ TH.FunD (TH.mkName (name d)) [ds]
     where
-      go :: ([Term], [Generator], Lang) -> Either Error TH.Clause
-      go (args, gens, body) = do
-        let p = map pterm args ++ map pgen gens
+      go :: Def -> Either Error TH.Clause
+      go (Def _ args gens body) = do
+        args <- mapM pvar args
+        gens <- mapM pgen gens
         b <- toQuote body
-        return $ TH.Clause p (TH.NormalB b) []
+        return $ TH.Clause (args ++ gens) (TH.NormalB b) []
 
+($:) :: TH.Exp -> [TH.Exp] -> TH.Exp
 ($:) = foldl TH.AppE
 
 instance Quotable Lang TH.Exp where
+  toQuote Empty = qname "mzero"
   toQuote (Call d name args gens) = do
     let immature = if d == Delayed then TH.AppE (TH.ConE $ TH.mkName "Immature") else id
-    let n = toLower name
+    n <- qname name
     xs <- mapM qvar args
     gs <- mapM qgen gens
-    return $ immature $ TH.VarE (TH.mkName n) $: (xs ++ gs) -- TODO: Test immature
-  toQuote (Gen g) = do
-    let n = toLower g
-    return $ TH.VarE (TH.mkName n)
+    return $ immature $ n $: (xs ++ gs) -- TODO: Test immature
+  toQuote (Gen g) = qgen g
   toQuote (Return exprs) = do
-    exprs <- mapM ((Just <$>) . toQuote) exprs
-    let e = case exprs of
-          [] -> TH.TupE []
-          [Just e] -> e
-          es -> TH.TupE es
-    return $ TH.VarE (TH.mkName "return") $: [e]
-  toQuote (Sum exprs) = do
+    fn <- qname "return"
     exprs <- mapM toQuote exprs
-    return $ TH.VarE (TH.mkName "msum") $: [TH.ListE exprs]
+    let e = case exprs of
+          [e] -> e
+          es -> TH.TupE (Just <$> es)
+    return $ fn $: [e]
+  toQuote (Sum exprs) = do
+    fn <- qname "msum"
+    exprs <- mapM toQuote exprs
+    return $ fn $: [TH.ListE exprs]
   toQuote (Match v branches) = do
+    fn <- qname "mzero"
     v' <- qvar v
     bs <- mapM go branches
-    let ot = TH.Match TH.WildP (TH.NormalB $ TH.VarE $ TH.mkName "mzero") []
+    let ot = TH.Match TH.WildP (TH.NormalB fn) []
     return $ TH.CaseE v' (bs ++ [ot])
     where
       go :: (Term, Lang) -> Either Error TH.Match
       go (patt, body) = do
-        let p = pterm patt
+        p <- pterm patt
         b <- toQuote body
         return $ TH.Match p (TH.NormalB b) []
   toQuote (Guard a b) = do
+    fn <- qname "guard"
+    eq <- qname "=="
     x <- qvar a
-    y <- qvar b
-    return $ TH.VarE (TH.mkName "guard") $: [TH.InfixE (Just x) (TH.VarE $ TH.mkName "==") (Just y)]
+    y <- toQuote b
+    return $ fn $: [TH.InfixE (Just x) eq (Just y)]
   toQuote (Bind [([], ret)]) = do
     toQuote ret
   toQuote (Bind stmts) = do
@@ -142,14 +151,14 @@ instance Quotable Lang TH.Exp where
     where
       go :: ([Var], Lang) -> Either Error TH.Stmt
       go (vars, Return exprs) | not (null vars) = do
-        let vs = map pvar vars
+        vs <- mapM pvar vars
         s <- mapM toQuote exprs
         if length vars == length exprs then
           return $ TH.LetS (zipWith (\n v -> TH.ValD n (TH.NormalB v) []) vs s)
         else
           Left ("Variable mismatch: " ++ show vars ++ " / " ++ show exprs)
       go (vars, stmt) = do
-        let vs = map (TH.VarP . TH.mkName . toLower) vars
+        vs <- mapM pvar vars
         s <- toQuote stmt
         return $ (case vs of
           [] -> TH.NoBindS
